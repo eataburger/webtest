2461ec6fe9b4df4f0402475afd3dc3eb7ff56ce5
Jurģis <84973391+eataburger@users.noreply.github.com>
2021-06-09 22:43:13 +0300

    These files didnt add before

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function ValidateEmail(inputText)
+{
+var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+if (inputText.value.length == 0) {
+alert("Email address is required");
+document.form1.text1.focus();
+return false;
+}
+else if (inputText.endsWith('.co')) {
+alert("We are not accepting subscriptions from Columbia emails");
+document.form1.text1.focus();
+return false;
+}
+else if (!inputText.value.match(mailformat)) {
+alert("Please provide a valid email address");
+document.form1.text1.focus();
+return false;
+}
+else {
+alert("Valid email address");
+document.form1.text1.focus();
+document.form1.submit();
+return true;
+
+}
+}
